9c7a8c11dce569a7c6b3e1778179035c46d11b27
Rob Godbey <rbgodbey@yahoo.com>
2024-12-11 10:43:15 -0500

    Added all 20 questions

--- src/quizData.ts
+++ src/quizData.ts
@@ -8,7 +8,7 @@ export const quizQuestions: QuizQuestion[] = [
   {
     question: "Roland Checked into the hotel under what name?",
     options: ["Damon Steel", "Dustin Steel", "Darren Steel", "Damian Steel"],
-    correctAnswer: 0
+    correctAnswer: 3
   },
   {
     question: "What is the name of the town where the show is set?",
@@ -17,9 +17,94 @@ export const quizQuestions: QuizQuestion[] = [
   },
   {
     question: "What is the name of the motel the Rose family lives in?",
-    options: ["Rosebud Motel", "Schitt's Inn", "Rose Motel", "Creek Lodge"],
+    options: ["Schitt's Inn", "Rosebud Motel", "Rose Motel", "Creek Lodge"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is the name of the owner of the pine cone farm?",
+    options: ["Petal", "Hooch", "Moonshine", "Peabody"],
+    correctAnswer: 2
+  },
+  {
+    question: "At the Estate sale, what did John want to bid on?",
+    options: ["Cuff Links", "Mattress", "Golf Clubs", "Vase"],
     correctAnswer: 0
-  }
+  },
+  {
+    question: "What was Mutt's girlfriend's name?",
+    options: ["Flower", "Tennessee", "Maryland", "Tallahassee"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is the Blouse Barn owner's name?",
+    options: ["Wendy", "Maryl", "Flora", "Betty"],
+    correctAnswer: 0
+  },
+  {
+    question: "What is the surname of the family around which the show is centred?",
+    options: ["Smith", "Schitt", "Rose", "Creek"],
+    correctAnswer: 2
+  },
+  {
+    question: "What is the name of Alexis's Greek boyfriend at the start of the show?",
+    options: ["Aristotle", "Hercules", "Apollo", "Stavros"],
+    correctAnswer: 3
+  },
+  {
+    question: "In order to make her ex-boyfriend jealous, what does Alexis do to Mutt the very first time she sees him?",
+    options: ["Punches him", "Kisses him", "Hugs him", "Smacks him"],
+    correctAnswer: 1
+  },
+  {
+    question: "Who is the mayor of Schitt's Creek throughout all six seasons?",
+    options: ["Ronald Schitt", "Roland Schitt", "Schitt Rodriguez", "Rosewood Schitt III"],
+    correctAnswer: 1
+  },
+  {
+    question: "What gift does Mutt give Alexis, much to her dismay and to David's delight?",
+    options: ["Ring", "Sweater", "Bicycle", "Plant"],
+    correctAnswer: 2
+  },
+  {
+    question: "Demonstrating how self-absorbed Moira had been prior to moving to Schitt's Creek, she cannot remember the middle name of which family member?",
+    options: ["Alexis", "David", "Johnny", "Roland"],
+    correctAnswer: 0
+  },
+  {
+    question: "What is the name of the Schitt's Creek's dining establishment which serves 'Meadow Harvest' smoothies?",
+    options: ["Rose Diner", "Schitt's Creek Diner", "Rosebud Cafe", "Tropical Cafe"],
+    correctAnswer: 3
+  },
+  {
+    question: "One of Alexis' main love interests is Ted Mullens. What is his occupation?",
+    options: ["Lumber Jack", "Massage Therapist", "Veterinarian", "Doctor"],
+    correctAnswer: 2
+  },
+  {
+    question: "Moira is hired as a spokesperson for Herb Ertlinger and asked to act in a few commercials. What barely potable product is she promoting?",
+    options: ["Fruit Wine", "Maple Syrup", "Moose Burgers", "Smoothies"],
+    correctAnswer: 0
+  },
+  {
+    question: "When Alexis receives her certificate for graduating from high school, what word is unfortunately (but humorously) spelled incorrectly?",
+    options: ["Graduation", "Public", "Marketing", "Relations"],
+    correctAnswer: 1
+  },
+  {
+    question: "Moira joins a singing group in Season 2. What is the name of the group?",
+    options: ["Schitt's Choir", "Fantastics", "Jazzagals", "Song Girls"],
+    correctAnswer: 2
+  },
+  {
+    question: "Moira claims to have made which dish for her family, all the time, using her mother's recipe?",
+    options: ["Pizza", "Lasagna", "Steak Diane", "Chicken Enchiladas"],
+    correctAnswer: 3
+  },
+  {
+    question: "In season 3, David undertakes a new business venture. What does he call it?",
+    options: ["Rose Apothecary", "Rose Dispensary", "Schitt's Creek Pizza", "Schitt's Creek Chicken"],
+    correctAnswer: 0
+  }  
   // Add more questions here to reach a total of 20
 ];
 
